df8b7c5e93fda525f8887caaea530b2d10492ffa
Mayur Soni <mayur2003soni@gmail.com>
2026-04-07 19:24:57 +0530

    Fix dropdown

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -87,16 +87,13 @@ const Navbar = () => {
             >HOME</Link>
           </li>
           <li className="nav-dropdown-wrapper">
-            <Link
-              href="/#solutions"
-              className={`nav-dropdown-trigger ${activeLink === '#solutions' ? 'active' : ''}`}
-              onClick={() => handleLinkClick('#solutions')}
+            <div className={"nav-dropdown-trigger"}
             >
               SOLUTIONS
               <svg className="dropdown-chevron" width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 1L5 5L9 1" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
-            </Link>
+            </div>
             <div className="nav-dropdown-menu">
               <div className="nav-dropdown-content">
                 {solutionsItems.map((item, idx) => (
